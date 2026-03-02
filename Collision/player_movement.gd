d4158f5e3ea5d1fcc63a1eb98dd7a30a46e89b2c
Jeremy P. Schneider <picrik@live.fr>
2026-03-02 23:03:57 +0100

    addingPlayer

---
+++ Collision/player_movement.gd
@@ -0,0 +1,9 @@
+extends RigidBody3D
+
+@export var move_speed : float = 2.0
+
+func _physics_process(delta: float) -> void:
+	if Input.is_physical_key_pressed(KEY_LEFT):
+		apply_force(Vector3.LEFT * move_speed)
+	if Input.is_physical_key_pressed(KEY_RIGHT):
+		apply_force(Vector3.RIGHT * move_speed)
